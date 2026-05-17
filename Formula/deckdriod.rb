19542b0067c101239eaf328d3195a40abc620d12
Asmat <as@Asmats-MacBook-Air.local>
2026-05-18 02:53:48 +0530

    Add DeckDriod formula

---
+++ Formula/deckdriod.rb
@@ -0,0 +1,26 @@
+class Deckdriod < Formula
+  desc "Professional Android Development Dashboard"
+  homepage "https://github.com/theasmat/deckdriod"
+  version "0.1.0" # Update this with your actual version
+
+  if OS.mac?
+    if Hardware::CPU.arm?
+      url "https://github.com/theasmat/deckdriod/releases/download/v#{version}/deckdriod-aarch64-apple-darwin.tar.gz"
+      # sha256 "REPLACE_WITH_ACTUAL_SHA256"
+    else
+      url "https://github.com/theasmat/deckdriod/releases/download/v#{version}/deckdriod-x86_64-apple-darwin.tar.gz"
+      # sha256 "REPLACE_WITH_ACTUAL_SHA256"
+    end
+  elsif OS.linux?
+    url "https://github.com/theasmat/deckdriod/releases/download/v#{version}/deckdriod-x86_64-unknown-linux-musl.tar.gz"
+    # sha256 "REPLACE_WITH_ACTUAL_SHA256"
+  end
+
+  def install
+    bin.install "deckdriod"
+  end
+
+  test do
+    system "#{bin}/deckdriod", "--help"
+  end
+end
