7dcd840ade39fcb3b0db979c593cb761add92fa9
Asmat <as@Asmats-MacBook-Air.local>
2026-05-18 06:40:33 +0530

    Update DeckDriod to v0.1.4 with SHA256 checksums

--- Formula/deckdriod.rb
+++ Formula/deckdriod.rb
@@ -1,19 +1,19 @@
 class Deckdriod < Formula
   desc "Professional Android Development Dashboard"
   homepage "https://github.com/theasmat/deckdriod"
-  version "0.1.4" # Update this with your actual version
+  version "0.1.4"
 
   if OS.mac?
     if Hardware::CPU.arm?
       url "https://github.com/theasmat/deckdriod/releases/download/v#{version}/deckdriod-aarch64-apple-darwin.tar.gz"
-      # sha256 "REPLACE_WITH_ACTUAL_SHA256"
+      sha256 "ce874ffc87fea1bbdc137a71c7fb78fef71d38ea4cf54b7f80519d89ce1bd342"
     else
       url "https://github.com/theasmat/deckdriod/releases/download/v#{version}/deckdriod-x86_64-apple-darwin.tar.gz"
-      # sha256 "REPLACE_WITH_ACTUAL_SHA256"
+      sha256 "47198ea60f74337ad9cdbab1b9a0991e3dba62ff6761b26d2c89cf51e4d5d193"
     end
   elsif OS.linux?
     url "https://github.com/theasmat/deckdriod/releases/download/v#{version}/deckdriod-x86_64-unknown-linux-musl.tar.gz"
-    # sha256 "REPLACE_WITH_ACTUAL_SHA256"
+    sha256 "5980fa5375b0aed8fa93314932ae5ea4d5502a4224dd4619572f3b4a9944eaf7"
   end
 
   def install
@@ -21,6 +21,6 @@ class Deckdriod < Formula
   end
 
   test do
-    system "#{bin}/deckdriod", "--help"
+    system "#{bin}/deckdriod", "--version"
   end
 end
